7ab68195b0db6f4eb7b90236f93cd98ccb097661
henriits <henritsarents@gmail.com>
2025-08-10 14:40:39 +0300

    feat: add vortex animation background component to whole application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import Hero from "./components/Hero";
@@ -7,22 +6,28 @@ import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import { Vortex } from "./components/ui/Vortex";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/hero" />} />
-          <Route path="hero" element={<Hero />} />
-          <Route path="about" element={<About />} />
-          <Route path="experience" element={<Experience />} />
-          <Route path="skills" element={<Skills />} />
-          <Route path="work" element={<Work />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Vortex
+      backgroundColor="black"
+      className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/hero" />} />
+            <Route path="hero" element={<Hero />} />
+            <Route path="about" element={<About />} />
+            <Route path="experience" element={<Experience />} />
+            <Route path="skills" element={<Skills />} />
+            <Route path="work" element={<Work />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Vortex>
   );
 }
 
