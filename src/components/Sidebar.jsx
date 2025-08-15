a9f8a09d4eaf047e717b38047631a937ce54182b
henriits <henritsarents@gmail.com>
2025-08-15 18:45:52 +0300

    fix: increase padding in Sidebar panel for improved layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -36,7 +36,7 @@ const Sidebar = () => {
       {/* Sidebar Panel */}
       {open && (
         <GlassCard className="fixed top-0 left-0 w-2/4 md:w-1/4 lg:w-1/5 h-full z-40 text-white">
-          <div className="flex flex-col justify-start items-start p-10 space-y-6 w-full">
+          <div className="flex flex-col justify-start items-start p-18 space-y-6 w-full">
             {sections.map((section) => (
               <NavLink
                 key={section}
